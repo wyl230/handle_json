cb016f02005d4ce3c2eb660178bf5f2e682fc3d5
wyl230 <1900012779@pku.edu.cn>
2023-03-21 23:17:22 +0800

    adjust

--- json_processing.py
+++ json_processing.py
@@ -5,7 +5,6 @@ class JSONProcessor:
         self.json_string = json.load(data)
         self.pens_list = self.json_string['pens']
 
-
     def cut_redundancy(self):
         # 后端需要恢复画布，所以前端传过来画布的所有信息。但是处理连接关系的时候不需要部分信息，这里去掉不需要的部分
         for i in self.pens_list:
@@ -30,24 +29,21 @@ class JSONProcessor:
 
     def get_modules_with_interfaces(self): # 返回模型字典，值为其拥有的的接口列表
         modules = {}
-        for pen in self.pens_list:
-            try:
-                pen['children'] # 含有children属性的为模型
-                modules[pen['id']] = []
+        for pen in self.get_modules():
+            pen['children'] # 含有children属性的为模型
+            modules[pen['id']] = []
 
-                # print('含有的所有接口如下:')
-                for child_id in pen['children']:
-                    child = filter(lambda x: x['id'] == child_id, self.pens_list)
-                    for i in child: # 符合条件的理论上只有一个
-                        modules[pen['id']].append(i)
-            except:
-                pass
+            # print('含有的所有接口如下:')
+            for child_id in pen['children']:
+                child = filter(lambda x: x['id'] == child_id, self.pens_list)
+                for i in child: # 符合条件的理论上只有一个
+                    modules[pen['id']].append(i)
         return modules
 
     def get_lines(self): # 返回原始的连线(惰性)
         return filter(lambda pen: pen['name'] == 'line', self.pens_list)
 
-    def get_lines_with_interfaces(self):
+    def get_lines_with_interfaces(self): # 返回连线（字典），值为其拥有的接口列表
         lines = {}
         # print('所有连线，给出了哪两个接口相连')
         for pen in filter(lambda pen:pen['name'] == 'line', self.pens_list):
